f734376cfd6de3a18b3f5f920cb1ba76cd5a2b91
noobilseuwu <152938395+noobilseuwu@users.noreply.github.com>
2024-01-10 13:22:48 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("policia").onclick = function(){
+    location.href="blog_policia.html";
+}
+
+document.getElementById("lana").onclick = function () {
+    location.href = "blog_lana.html";
+}
